7e9d807a32b584172f50f131cb815955dedd4382
rithik112 <rithiksarab112@gmail.com>
2020-10-11 15:06:54 +0530

    Connection Established With MongoDB and Server

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const mongoose = require('mongoose')
+const db = "mongodb+srv://Auth1:Auth1@cluster0.4a0ry.mongodb.net/Auth1?retryWrites=true&w=majority"
+
+mongoose.connect(db, err => {
+    if (err) {
+        console.error("Error While Connecting To Database...!\n" + err)
+    } else {
+        console.log('Connected Successfully to MongoDB...!')
+    }
+})
+
 
 router.get('/',  (req, res) => {
     res.send("Hey It's me Api...!")
