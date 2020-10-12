8abd53b00e07180afa9b5ad8dd3c8984473f481e
rithik112 <rithiksarab112@gmail.com>
2020-10-13 01:55:28 +0530

    Cors Middleware Added

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const cors = require('cors')
+const cors = require('cors')
 
 const PORT = 3000
 const api = require("./routes/api")
 
 const app = express()
-// app.use(cors())
+app.use(cors())
 
 app.use(bodyParser.json())
 
