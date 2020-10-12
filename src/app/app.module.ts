2167b30d977abb1f0b46ecdf7d66f8d7ab508af9
rithik112 <rithiksarab112@gmail.com>
2020-10-13 01:16:08 +0530

    Minor Bug Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -33,11 +34,11 @@ import { FaqComponent } from './components/faq/faq.component';
     ProfileSavedListingsComponent,
     ListingDetailsComponent,
     FaqComponent,
-    FormsModule,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
