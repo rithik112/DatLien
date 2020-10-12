cf51dec792f029a3c403ee198b81d1b94992a472
rithik112 <rithiksarab112@gmail.com>
2020-10-13 02:41:51 +0530

    LogIn Method With DB Added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUserData = { email: "", password: "" }
+
+  constructor(private _auth: AuthService,
+    private _router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  loginUser() {
+    this._auth.loginUser(this.loginUserData)
+    .subscribe(
+      res => {
+        localStorage.setItem('token', res.token)
+        this._router.navigate(['/submitlisting'])
+      },
+      err => console.log(err)
+    )
   }
 
 }
