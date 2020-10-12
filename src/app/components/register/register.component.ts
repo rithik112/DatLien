abd011a47d152ade02430851303c47ce9b01983b
rithik112 <rithiksarab112@gmail.com>
2020-10-13 02:10:27 +0530

    Registration Method With DB Added

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerUserData = { email: "", password: "" }
+
+  constructor(private _auth: AuthService,
+    private _router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  registerUser() {
+    this._auth.registerUser(this.registerUserData)
+    .subscribe(
+      res => {
+        localStorage.setItem('token', res.token)
+        this._router.navigate(['/home'])
+      },
+      err => console.log(err)
+    ) 
   }
 
 }
