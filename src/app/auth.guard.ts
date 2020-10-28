49ffd271f45423744649410feba749cfcf77885f
rithik112 <rithiksarab112@gmail.com>
2020-10-29 00:48:19 +0530

    Added canActivate Method and Router

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './Services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
   
+  constructor(private _authService: AuthService,
+    private _router: Router) { }
+  
+  canActivate(): boolean {
+    if (this._authService.loggedIn()) {
+      console.log('true')
+      return true
+    } else {
+      console.log('false')            
+      this._router.navigate(['/login'])
+      return false
+    }
+  }
+
 }
