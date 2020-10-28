cf90afb1dba22e430724b2aed9a53ae21235d18c
rithik112 <rithiksarab112@gmail.com>
2020-10-28 22:57:40 +0530

    Added Get Methods

--- src/app/Services/listing.service.ts
+++ src/app/Services/listing.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListingService {
 
-  constructor() { }
+  private _listingUrl = "http://localhost:3000/api/listings";
+  private _royalListingUrl = "http://localhost:3000/api/royal";
+
+  constructor(private http: HttpClient) { }
+  
+  getListings() {
+    return this.http.get<any>(this._listingUrl)
+  }
+
+  getRoyalListings() {
+    return this.http.get<any>(this._royalListingUrl)
+  }
+
 }
